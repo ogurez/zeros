047d94fb50e4977c13c99841525e6761343d9bb3
Ray_Bruges <ogurezslava@gmail.com>
2018-09-24 16:09:31 +0300

    !

--- src/index.js
+++ src/index.js
@@ -11,16 +11,3 @@ module.exports = function getZerosCount(number) {
     }
     return res;
 }
-
-
-module.exports = function getZerosCount(number) {
-    result = 0
-    for (var i = 1; i <= number; i++) {
-        temp = i
-        while (temp%5 == 0){ 
-              temp /= 5
-              result++
-        }
-    }
-    return result
- }
