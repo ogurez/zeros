7710998857c3e5432b00a1180b8d104476252a85
Ray_Bruges <ogurezslava@gmail.com>
2018-09-24 15:58:05 +0300

    first-try

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let res=0;
+    let prom=0;
+
+	for(let i=0;i<=number;i++){
+        prom=i;
+	    while(prom%5==0){
+            res++;
+            prom=prom/5;
+    }
+             return Math.floor(prom);
+    }
 }
