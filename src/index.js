acc7a5d756b32790b0b3851b9d852f02df833ce8
Ray_Bruges <ogurezslava@gmail.com>
2018-09-24 16:08:56 +0300

    fixed3

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function getZerosCount(number) {
     let res=0;
     let prom=0;
 
-	for(let i=0;i<=number;i++){
+	for(let i=1;i<=number;i++){
       prom=i;
 	    while(prom%5==0){
             prom=prom/5;
@@ -11,3 +11,16 @@ module.exports = function getZerosCount(number) {
     }
     return res;
 }
+
+
+module.exports = function getZerosCount(number) {
+    result = 0
+    for (var i = 1; i <= number; i++) {
+        temp = i
+        while (temp%5 == 0){ 
+              temp /= 5
+              result++
+        }
+    }
+    return result
+ }
